d4e655ba62c9e8776adf12a066625dd4e3c97097
mugabi0978 <powellorenz@gmail.com>
2016-12-01 09:31:43 +0300

    Footer and Header Updates
    form validation, calendar $ time pickers, plus Browsing abiliity
    included

--- farm2/includes/header.php
+++ farm2/includes/header.php
@@ -4,11 +4,82 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="description" content="">
 
-        <meta name="author" content="Coderthemes">
+
+        <meta name="author" content="">
 
         <script src="assets/js/jquery.min.js"></script>
+
+        <script src="plugins/timepicker/bootstrap-timepicker.js"></script>
+        <script src="plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+        <script src="plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+        <script src="plugins/clockpicker/js/bootstrap-clockpicker.min.js"></script>
+        <script src="plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+       <!--  <script src="plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script> -->
+        <!-- <script src="plugins/bootstrap-daterangepicker/daterangepicker.js"></script> -->
+
+
+        <!-- <script src="assets/js/jquery.min.js"></script> -->
+
+        
+<!-- 
+         <link href="jQuery.filer/css/jquery.filer.css" type="text/css" rel="stylesheet" />
+
+         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+ -->
+
+        <link rel="shortcut icon" href="assets/images/underscore21.png">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> 
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
+        <link rel="stylesheet" href="plugins/tooltipster/tooltipster.bundle.min.css">
+        <!-- 
+ <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+ <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script> -->
+
+<!-- Google Fonts -->
+ <!--    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+ -->
+    <!-- Styles -->
+    <link href="jQuery.filer/css/jquery.filer.css" rel="stylesheet">
+
+    <script src="http://code.jquery.com/jquery-3.1.0.min.js"></script>
+ <!--    <script src="jQuery.filer/js/jquery.filer.min.js"></script> -->
+        
+
+    <!-- Jvascript -->
+    <!-- <script src="http://code.jquery.com/jquery-3.1.0.min.js" crossorigin="anonymous"></script> -->
+    <script src="jQuery.filer/js/jquery.filer.js" type="text/javascript"></script>
+ <!--    <script src="jQuery.filer/js/custom.js" type="text/javascript"></script> -->
+
+
+
+
+
+
+
+        
+       <!--  <script src="jQuery.filer/js/jquery.filer.min.js"></script>
+        <script src="jQuery.filer/js/jquery.filer.js"></script>  -->
+
+           <!-- Jquery filer css -->
+        <!--<link href="plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
+        <link href="plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />-->
+
+
+
+    <!-- Jvascript -->
+    <!-- <script src="http://code.jquery.com/jquery-3.1.0.min.js"></script> -->
+    
+   <!--  <script src="jQuery.filer/js/custom.js" type="text/javascript"></script> -->
+
+       
 
 
           <!-- This is what you need -->
@@ -16,28 +87,28 @@
   <link rel="stylesheet" href="dist/sweetalert.css">
         <!-- App favicon -->
 
-        <link rel="shortcut icon" href="assets/images/underscore21.png">
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 
-        <link rel="stylesheet" href="plugins/tooltipster/tooltipster.bundle.min.css">
-
-         <!-- Jquery filer css -->
-        <link href="plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
-        <link href="plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
-
+      
 
         <!-- App title -->
 
-        <title></title>
+        
+        <!-- Google Fonts -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet"> -->
 
+    <!-- Styles -->
+    <!-- <link href="jQuery.filer/css/jquery.filer.css" rel="stylesheet"> -->
+
+    <!-- Jvascript -->
+   <!--  <script src="http://code.jquery.com/jquery-3.1.0.min.js" crossorigin="anonymous"></script> -->
+   <!--  <script src="jQuery.filer/js/jquery.filer.js" type="text/javascript"></script> -->
+   <!--  <script src="jQuery.filer/js/custom.js" type="text/javascript"></script>
+ -->
 
 
         <!--Morris Chart CSS -->
 
-		<link rel="stylesheet" href="./plugins/morris/morris.css">
+        <link rel="stylesheet" href="./plugins/morris/morris.css">
 
 
 
@@ -57,7 +128,7 @@
 
         <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
 
-		<link rel="stylesheet" href="plugins/switchery/switchery.min.css">
+        <link rel="stylesheet" href="plugins/switchery/switchery.min.css">
 
         <!-- Sweet Alert -->
         <link href="plugins/bootstrap-sweetalert/sweet-alert.css" rel="stylesheet" type="text/css">
@@ -77,7 +148,10 @@
 
 
 
-        <script src="assets/js/modernizr.min.js"></script>
+      <script src="assets/js/modernizr.min.js"></script>
+
+
+
 
 
 
